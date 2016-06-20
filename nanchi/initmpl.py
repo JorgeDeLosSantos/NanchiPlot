df2b9c335f5311134e6c8f509aafb5316f5f21e5
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-06-20 15:08:14 -0500

    Minor changes

--- nanchi/initmpl.py
+++ nanchi/initmpl.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm # Colormap
 from _const_ import *
 
-DEFAULT_STYLE = DARK_STYLE
+# Default style for graphs
+DEFAULT_STYLE = WHITE_STYLE
 
 if True:
     """
